ef416abad3050d203be5e10e716f481b2fa69ec3
Plinio Balduino <pbalduino+github@gmail.com>
2011-10-04 21:04:58 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parsec::Application.config.secret_token = '473a45632ec1ee55346337084c61ed3fd1e281ec418fb2666ba199485733e5058ce6c3d6f7d854f0d69af55b91341002a909d8945a287faba03f340f7f68f791'
